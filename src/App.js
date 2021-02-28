ac03043571bde7b95b87b5617f13285bcab730f0
Denis Dimchev <denis.dimchev@gmail.com>
2021-02-28 13:10:24 +0100

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css'
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm/>
+
     return (
         <ChatEngine
             height="100vh"
             projectID="f0ba8040-e7d4-4045-a012-9815d248e4e0"
-            userName="admin"
-            userSecret="12341234"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps }/>}
         />
     )
--- src/components/ReceivedMessage.jsx
+++ src/components/ReceivedMessage.jsx
@@ -1,29 +1,28 @@
-const ReceivedMessage = ( lastMessage, message ) => {
+const ReceivedMessage = ({ lastMessage, message }) => {
     const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
     return (
         <div className="message-row">
             {isFirstMessageByUser && (
                 <div 
                     className="message-avatar"
-                    style={{backgroundImage: message.sender && `url(${message?.sender?.avatar})`}}
+                    style={{backgroundImage: message.sender && `url(${message.sender.avatar})` }}
                 />
             )}
-            {message?.attachments?.length > 0 
+            {message.attachments && message.attachments.length > 0 
                 ? (
                     <img 
                         src={message.attachments[0].file}
-                        alt="message-attachments"
+                        alt="message-attachment"
                         className="message-image"
-                        style={{marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                        style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
                     />
                 ) : (
                     <div className="message" style={{float: 'left', marginLeft: isFirstMessageByUser ? '4px' : '48px', backgroundColor: '#CABCDC' }}>
                         {message.text}
                     </div>
                 )}
-    
         </div>
-    )
-}
+    );
+};
 
 export default ReceivedMessage;
